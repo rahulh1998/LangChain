f4b924cf92e7185a35e7f4371c35bf6e02e95dc5
rahulh1998 <rahul.h1998@gmail.com>
2025-09-11 00:52:18 +0530

    Utilising  Gemma3 model with Ollama and creating a simple Q&A App

---
+++ GenAI Applications/App2 Using Ollama - Gemma3.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+# Set the page layout to wide
+st.set_page_config(layout="wide")
+
+# Langsmith Tracing
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ('system','You are an expert in answering. Answer the questions in simple to understand manner.'),
+        ('user','Questions{questions}')
+    ])
+
+
+# Streamlit Framework
+st.title("First GenAI App Using Ollama using Deepseek-R1 7B")
+question = st.text_input("Ask your question ..")
+
+# Initializing LLM 
+llm = Ollama(model = 'gemma3:4b')
+
+# Output Parser
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if question:
+    response = chain.invoke(question)
+    st.write(response)
+
