4c33ce36dfcefde66a4126eeeaae60fe9c1bcf0b
rahulh1998 <rahul.h1998@gmail.com>
2025-09-19 07:26:27 +0530

    App2

--- GenAI Applications/App2 Using Ollama - Gemma3.py
+++ GenAI Applications/App2 Using Ollama - Gemma3.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_community.llms import Ollama
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
